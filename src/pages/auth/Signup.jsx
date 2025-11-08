3758edfab551545fbb6a1e6ee2228769ab4e8c88
muheey <54740134+AbdullahiMuhammad@users.noreply.github.com>
2025-11-08 08:14:17 +0100

    Update Signup.jsx

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import InputField from "../../component/input/InputField";
